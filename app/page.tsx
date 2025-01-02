c7788550df6a96c67671be25514b7ec3766b6a73
Faixan6190 <faixan6190@gmail.com>
2025-01-02 20:38:10 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import { Hero } from "@/components";
 
 export default function Home() {
-  return(
+  return (
     <main className="">
-
+      <Hero />
     </main>
-  )
+  );
 }
